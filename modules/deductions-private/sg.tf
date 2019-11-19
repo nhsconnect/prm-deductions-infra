31b69e7d91b08e53d12505a51fa1cf754b4eefea
Phil Evans <phil.evans@thoughtworks.com>
2019-11-19 16:14:15 +0000

    [PRMT-721] Allow Fargate service access to the internet to pull ECR image

--- modules/deductions-private/sg.tf
+++ modules/deductions-private/sg.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "ecs-tasks-sg" {
     name        = "${var.environment}-${var.component_name}-ecs-tasks-sg"
     vpc_id      = "${module.vpc.vpc_id}"
 
+    egress {
+        protocol    = "-1"
+        from_port   = 0
+        to_port     = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = {
         Name = "${var.environment}-${var.component_name}-ecs-tasks-sg"
     }    
