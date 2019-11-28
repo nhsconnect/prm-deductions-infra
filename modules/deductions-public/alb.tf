23a01169cb5435a4aa6e0991e34d4f8cc536da8e
Phil Evans <phil.evans@thoughtworks.com>
2019-11-28 10:33:27 +0000

    [PRMT-721] Add dep check stage to PDS Adaptor

--- modules/deductions-public/alb.tf
+++ modules/deductions-public/alb.tf
@@ -35,7 +35,7 @@ resource "aws_alb_listener" "alb-listener-https" {
   port              = "443"
   protocol          = "HTTPS"
 
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
   certificate_arn   = data.aws_acm_certificate.portal_acm_certificate.arn
 
   default_action {
