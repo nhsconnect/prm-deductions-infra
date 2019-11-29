0e4cfb9af91a09384041b2d022685161bb667074
Amy Welch <awelch@thoughtworks.com>
2019-11-29 10:36:43 +0000

    [PRMT-698] add stomp queue endpoint in parameter store

--- modules/deductions-private/mq.tf
+++ modules/deductions-private/mq.tf
@@ -38,7 +38,7 @@ resource "aws_ssm_parameter" "amqp-endpoint-0" {
   name        = "/NHS/${var.environment}-${data.aws_caller_identity.current.account_id}/amqp-endpoint/0"
   description = "AMQP endpoint to MQ broker. Index: 0"
   type        = "String"
-  value       = aws_mq_broker.deductor_mq_broker.instances.0.endpoints.0
+  value       = aws_mq_broker.deductor_mq_broker.instances.0.endpoints.1
 
   tags = {
     Environment = var.environment
@@ -49,7 +49,29 @@ resource "aws_ssm_parameter" "amqp-endpoint-1" {
   name        = "/NHS/${var.environment}-${data.aws_caller_identity.current.account_id}/amqp-endpoint/1"
   description = "AMQP endpoint to MQ broker. Index: 1"
   type        = "String"
-  value       = aws_mq_broker.deductor_mq_broker.instances.1.endpoints.0
+  value       = aws_mq_broker.deductor_mq_broker.instances.1.endpoints.1
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_ssm_parameter" "stomp-endpoint-0" {
+  name        = "/NHS/${var.environment}-${data.aws_caller_identity.current.account_id}/stomp-endpoint/0"
+  description = "STOMP endpoint to MQ broker. Index: 0"
+  type        = "String"
+  value       = aws_mq_broker.deductor_mq_broker.instances.0.endpoints.2
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_ssm_parameter" "stomp-endpoint-1" {
+  name        = "/NHS/${var.environment}-${data.aws_caller_identity.current.account_id}/stomp-endpoint/1"
+  description = "STOMP endpoint to MQ broker. Index: 1"
+  type        = "String"
+  value       = aws_mq_broker.deductor_mq_broker.instances.1.endpoints.2
 
   tags = {
     Environment = var.environment
