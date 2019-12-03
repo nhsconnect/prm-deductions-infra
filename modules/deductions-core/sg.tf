9cf484524180a189114b82b04ee7d9cb9e828f98
Phil Evans <phil.evans@thoughtworks.com>
2019-12-03 00:22:29 +0000

    [PRMT-760] Pipeline for Deductions Core

--- modules/deductions-core/sg.tf
+++ modules/deductions-core/sg.tf
@@ -2,21 +2,13 @@ resource "aws_security_group" "ecs-tasks-sg" {
     name        = "${var.environment}-${var.component_name}-ecs-tasks-sg"
     vpc_id      = module.vpc.vpc_id
 
-    # ingress {
-    #     description     = "Allow traffic from ALB to PDS Adaptor"
-    #     protocol        = "tcp"
-    #     from_port       = "3000"
-    #     to_port         = "3000"
-    #     security_groups = [aws_security_group.pds-adaptor-lb-sg.id]
-    # }
-
-    # ingress {
-    #     description     = "Allow traffic from ALB to GP2GP Adaptor"
-    #     protocol        = "tcp"
-    #     from_port       = "80"
-    #     to_port         = "80"
-    #     security_groups = [aws_security_group.pds-adaptor-lb-sg.id]
-    # }
+    ingress {
+        description     = "Allow traffic from ALB to PDS Adaptor"
+        protocol        = "tcp"
+        from_port       = "3000"
+        to_port         = "3000"
+        security_groups = [aws_security_group.core-alb-sg.id]
+    }
 
     egress {
         description = "Allow All Outbound"
@@ -34,6 +26,14 @@ resource "aws_security_group" "ecs-tasks-sg" {
 resource "aws_security_group" "db-sg" {
     name        = "db-sg"
     vpc_id      = module.vpc.vpc_id
+
+    ingress {
+        description     = "Allow traffic from ALB to GP2GP Adaptor"
+        protocol        = "tcp"
+        from_port       = "5432"
+        to_port         = "5432"
+        security_groups = [aws_security_group.ecs-tasks-sg.id]
+    }
 
     tags = {
         Name = "db-sg"
