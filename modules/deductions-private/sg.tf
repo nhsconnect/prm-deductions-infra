ffcd1509957e3a0aeb8631111b3c69ff44fb2a9e
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-03 16:32:38 +0000

    [PRMT-720] Allow MHS subnets to connect with MQ

--- modules/deductions-private/sg.tf
+++ modules/deductions-private/sg.tf
@@ -2,33 +2,49 @@ resource "aws_security_group" "mq_sg" {
     vpc_id = module.vpc.vpc_id
     name   = "deductor-mq-sg"
 
-    ingress {
-        description         = "Access to Deductions Private ECS Tasks"
-        protocol            = "tcp"
-        from_port           = "61614"
-        to_port             = "61614"
-        security_groups     = [aws_security_group.ecs-tasks-sg.id]
-    }
-
-    ingress {
-        description         = "Access to MQ Admin Console NLB"
-        protocol            = "tcp"
-        from_port           = "8162"
-        to_port             = "8162"
-        cidr_blocks         = ["10.20.101.113/32", "10.20.102.41/32"]      
-    } 
-
-    egress {
-        description         = "Allow All Outbound"
-        protocol            = "tcp" 
-        from_port           = "0"
-        to_port             = "0"
-        cidr_blocks         = ["0.0.0.0/0"]      
-    }         
-
   tags = {
       Name = "deductor-mq-b-sg"
-  }    
+  }
+}
+
+resource "aws_security_group_rule" "ingress_ecs_tasks" {
+  type                = "ingress"
+  security_group_id   = aws_security_group.mq_sg.id
+  description         = "Access to Deductions Private ECS Tasks"
+  protocol            = "tcp"
+  from_port           = "61614"
+  to_port             = "61614"
+  source_security_group_id     = aws_security_group.ecs-tasks-sg.id
+}
+
+resource "aws_security_group_rule" "ingress_console_nlb" {
+  type                = "ingress"
+  security_group_id   = aws_security_group.mq_sg.id
+  description         = "Access to MQ Admin Console NLB"
+  protocol            = "tcp"
+  from_port           = "8162"
+  to_port             = "8162"
+  cidr_blocks         = ["10.20.101.113/32", "10.20.102.41/32"]
+}
+
+resource "aws_security_group_rule" "ingress_mhs" {
+  type                = "ingress"
+  security_group_id   = aws_security_group.mq_sg.id
+  description         = "Access to queues from MHS VPC"
+  protocol            = "tcp"
+  from_port           = "5671"
+  to_port             = "5671"
+  cidr_blocks         = ["10.33.0.0/16"]
+}
+
+resource "aws_security_group_rule" "egress_all" {
+  type                = "egress"
+  security_group_id   = aws_security_group.mq_sg.id
+  description         = "Allow All Outbound"
+  protocol            = "tcp"
+  from_port           = "0"
+  to_port             = "0"
+  cidr_blocks         = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "ecs-tasks-sg" {
@@ -61,7 +77,7 @@ resource "aws_security_group" "ecs-tasks-sg" {
 
     tags = {
         Name = "${var.environment}-${var.component_name}-ecs-tasks-sg"
-    }    
+    }
 }
 
 resource "aws_security_group" "pds-adaptor-lb-sg" {
