e2365d7c8cbcb54139186efd74a28267a5a24e26
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-09 17:27:01 +0000

    [PRMT-760] Setup health check for EHR

--- modules/deductions-core/alb.tf
+++ modules/deductions-core/alb.tf
@@ -1,6 +1,6 @@
 resource "aws_alb" "alb" {
   name            = "${var.environment}-${var.component_name}-alb"
-  subnets         = module.vpc.public_subnets 
+  subnets         = module.vpc.public_subnets
   security_groups = [aws_security_group.core-alb-sg.id]
 }
 
@@ -10,6 +10,14 @@ resource "aws_alb_target_group" "ehr-repo-alb-tg" {
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
   target_type = "ip"
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    interval            = 10
+    path                = "/health"
+    port                = 3000
+  }
 }
 
 # Redirect all traffic from the ALB to the target group
