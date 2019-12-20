3e092134ce461225d858bf274288a4542f38b17e
Phil Evans <phil.evans@thoughtworks.com>
2019-12-20 13:52:35 +0000

    [PRMT-773] Moving ALB TG and Listener to the component repo

--- modules/deductions-public/data.tf
+++ modules/deductions-public/data.tf
@@ -7,8 +7,3 @@ data "aws_ssm_parameter" "deductions_private_bastion" {
 }
 
 data "aws_caller_identity" "current" {}
-
-data "aws_acm_certificate" "portal_acm_certificate" {
-  domain   = "${var.environment}.patient-deductions.nhs.uk"
-  statuses = ["ISSUED"]
-}
