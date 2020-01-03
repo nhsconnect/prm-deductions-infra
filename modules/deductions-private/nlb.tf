08b92beb424e898478290c03d5115132ead178eb
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-03 17:07:32 +0000

    [PRMT-818] make nlb listener name unique per env

--- modules/deductions-private/nlb.tf
+++ modules/deductions-private/nlb.tf
@@ -24,7 +24,7 @@ resource "aws_lb_listener" "deductor_mq_console_nlb_listener" {
 }
 
 resource "aws_lb_target_group" "mq_console_nlb_listener_tg" {
-  name          = "mq-console-nlb-listener-tg"
+  name          = "${var.environment}-mq-console-nlb-listener-tg"
   port          = 8162
   protocol      = "TCP"
   vpc_id        = module.vpc.vpc_id
@@ -38,5 +38,5 @@ resource "aws_lb_target_group_attachment" "attachment" {
     target_id        = element(aws_mq_broker.deductor_mq_broker.instances.*.ip_address, count.index)
     port             = 8162
 
-    count = var.mq_allow_public_console_access == true ? 2 : 0  
+    count = var.mq_allow_public_console_access == true ? 2 : 0
 }
