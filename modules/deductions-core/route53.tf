ebd8b7ac9702fbb26808b0319adb25e71dedcd6e
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-17 16:33:13 +0000

    Added DNS record for ALB

--- modules/deductions-core/route53.tf
+++ modules/deductions-core/route53.tf
@@ -5,3 +5,11 @@ resource "aws_route53_record" "mq-console-r53-record" {
   ttl     = "300"
   records = [aws_alb.alb.dns_name]
 }
+
+resource "aws_route53_record" "alb-r53-record" {
+  zone_id = var.private_zone_id
+  name    = "${var.environment}.alb"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_alb.alb-internal.dns_name]
+}
