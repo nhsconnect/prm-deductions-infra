514575a2ceb9fbec461524a825c315dce4062d3f
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2020-01-20 09:05:05 +0000

    Shorten the name of internal ALB
    It cannot be longer that 32 characters and could not be deployed in test
    environment

--- modules/deductions-core/alb-internal.tf
+++ modules/deductions-core/alb-internal.tf
@@ -1,5 +1,5 @@
 resource "aws_alb" "alb-internal" {
-  name            = "${var.environment}-${var.component_name}-alb-internal"
+  name            = "${var.environment}-${var.component_name}-alb-int"
   subnets         = module.vpc.private_subnets
   security_groups = [aws_security_group.core-alb-internal-sg.id]
   internal        = true
