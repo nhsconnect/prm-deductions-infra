c8b7e26f227b94115b52b4b4b5b78cfe1f8b7832
aiwatko <oktawia.kata1@nhs.net>
2021-04-29 10:26:59 +0100

    [PRMT-1990] Transferred gp-to-repo certificates to gp-to-repo repository

--- modules/deductions-private/alb-internal.tf
+++ modules/deductions-private/alb-internal.tf
@@ -124,11 +124,6 @@ resource "aws_alb_target_group_attachment" "mq-attachment" {
     port             = 8162
 }
 
-resource "aws_lb_listener_certificate" "gp-to-repo-int-listener-cert" {
-  listener_arn    = aws_alb_listener.int-alb-listener-https.arn
-  certificate_arn = aws_acm_certificate_validation.gp-to-repo-cert-validation.certificate_arn
-}
-
 resource "aws_lb_listener_certificate" "generic-component-int-listener-cert" {
   listener_arn    = aws_alb_listener.int-alb-listener-https.arn
   certificate_arn = aws_acm_certificate_validation.generic-component-cert-validation.certificate_arn
