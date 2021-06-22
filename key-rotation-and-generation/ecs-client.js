9839fc0ef4e6884bcac909a290bd5eed2c164c8d
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-06-22 14:54:39 +0100

    [PRMT-2044] Added cluster to update ecs service command and more logging for debugging

--- key-rotation-and-generation/ecs-client.js
+++ key-rotation-and-generation/ecs-client.js
@@ -4,6 +4,9 @@ import {initializeConfig} from "./config";
 const client = new ECSClient({ region: "eu-west-2" });
 
 export const restartECS = async (serviceName) => {
-  const command = new UpdateServiceCommand({ forceNewDeployment: true, service: `${initializeConfig().nhsEnvironment}-${serviceName}-service`});
+  const service = `${initializeConfig().nhsEnvironment}-${serviceName}-service`
+  const cluster = `${initializeConfig().nhsEnvironment}-${serviceName}-ecs-cluster`
+  const command = new UpdateServiceCommand({ forceNewDeployment: true, service, cluster });
+  console.log(`Restarting ECS For Cluster ${cluster}, Service: ${service}`);
   await client.send(command);
 }
