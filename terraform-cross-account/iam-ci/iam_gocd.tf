fd810ad2e572a76b712363ce77242fecee2093cf
Oktawia Kata <oktawia.kata1@nhs.net>
2021-06-29 16:56:19 +0100

    [PRMT-1733] Created instance profiles to attach iam role to ec2 instances

--- terraform-cross-account/iam-ci/iam_gocd.tf
+++ terraform-cross-account/iam-ci/iam_gocd.tf
@@ -17,6 +17,10 @@ resource "aws_iam_role" "ci_agent" {
   assume_role_policy = data.aws_iam_policy_document.gocd_trust_policy.json
 }
 
+resource "aws_iam_instance_profile" "ci_agent" {
+  name = "repository-ci-agent"
+  role = aws_iam_role.ci_agent.name
+}
 
 resource "aws_iam_role_policy_attachment" "ci_agent" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
--- terraform-cross-account/iam-environment/iam_gocd.tf
+++ terraform-cross-account/iam-environment/iam_gocd.tf
@@ -19,6 +19,11 @@ resource "aws_iam_role" "ci_agent" {
   assume_role_policy = data.aws_iam_policy_document.gocd_trust_policy.json
 }
 
+resource "aws_iam_instance_profile" "ci_agent" {
+  name = "repository-ci-agent"
+  role = aws_iam_role.ci_agent.name
+}
+
 resource "aws_iam_role_policy_attachment" "admin_access" {
   role       = aws_iam_role.ci_agent.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
