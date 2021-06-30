63535aee75bbcebad31bfd1dccf7978e7d37f3ea
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-06-30 17:50:54 +0100

    [PRMT-2045] Wait for ECS services to restart after generating/rotating keys

--- key-rotation-and-generation/aws-clients/ecs-client.js
+++ key-rotation-and-generation/aws-clients/ecs-client.js
@@ -1,4 +1,8 @@
-import { ECSClient, UpdateServiceCommand } from "@aws-sdk/client-ecs";
+import {
+  DescribeServicesCommand,
+  ECSClient,
+  UpdateServiceCommand
+} from "@aws-sdk/client-ecs";
 import { initializeConfig } from "../config";
 
 const client = new ECSClient({ region: "eu-west-2" });
@@ -6,7 +10,30 @@ const client = new ECSClient({ region: "eu-west-2" });
 export const restartECS = async (serviceName) => {
   const service = `${initializeConfig().nhsEnvironment}-${serviceName}-service`
   const cluster = `${initializeConfig().nhsEnvironment}-${serviceName}-ecs-cluster`
-  const command = new UpdateServiceCommand({ forceNewDeployment: true, service, cluster });
+  const updateServiceCommand = new UpdateServiceCommand({ forceNewDeployment: true, service, cluster });
   console.log(`Restarting ECS For Cluster ${cluster}, Service: ${service}`);
-  await client.send(command);
+  await client.send(updateServiceCommand);
+
+  await waitForServiceToRestart(service, cluster);
 }
+
+const waitForServiceToRestart = async (service, cluster) => {
+  const describeServicesCommand = new DescribeServicesCommand({services: [service], cluster});
+
+  const RETRY_COUNT = 10;
+  const POLLING_INTERVAL = 20000;
+  for (let i = 0; i < RETRY_COUNT; i++) {
+    const response = await client.send(describeServicesCommand);
+    const deployments = response.services[0].deployments;
+    console.log('try: ', i, 'deployments status:', deployments.map(deployment => deployment.status))
+    if (deployments.every(deployment => deployment.status !== 'ACTIVE')) {
+      console.log('No ACTIVE deployments. Assuming restart finished')
+      break;
+    }
+    await sleep(POLLING_INTERVAL);
+  }
+}
+
+const sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
