55241d518520629b87b8cc212786608e34b530c7
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-07-07 15:40:26 +0100

    [PRMT-2097] Used specific nat-gateway for internet route and spine internet route

--- terraform/modules/mhs/cluster-network/route-table.tf
+++ terraform/modules/mhs/cluster-network/route-table.tf
@@ -8,10 +8,9 @@ resource "aws_route_table" "mhs" {
 }
 
 resource "aws_route" "internet" {
-  count = 3
   route_table_id =   aws_route_table.mhs.id
   destination_cidr_block =  "0.0.0.0/0"
-  nat_gateway_id = var.mhs_nat_gateway_id[count.index]
+  nat_gateway_id = var.mhs_nat_gateway_id[0]
 }
 
 resource "aws_route" "deductions_private" {
