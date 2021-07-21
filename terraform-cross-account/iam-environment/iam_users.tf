8392b8c71e91d5ece84b4670aa4322b87c09b72c
Oktawia Kata <oktawia.kata1@nhs.net>
2021-07-21 13:52:09 +0100

    [PRMT-1735] Defined ssm parameters' policy for bootstrap admin role

--- terraform-cross-account/iam-environment/iam_users.tf
+++ terraform-cross-account/iam-environment/iam_users.tf
@@ -28,3 +28,45 @@ resource "aws_iam_role" "bootstrap_admin" {
   name = "BootstrapAdmin"
   assume_role_policy = data.aws_iam_policy_document.trust_policy.json
 }
+
+data "aws_iam_policy_document" "bootstrap_admin_permissions_policy" {
+  count = var.provision_strict_iam_roles ? 1 : 0
+  statement {
+    effect = "Allow"
+    actions = ["ssm:PutParameter*"]
+    resources = [
+      "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/repo/*/user-input/external/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = ["ssm:GetParameter*"]
+    resources = [
+      "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/repo/*/output/*",
+      "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/repo/output/*",
+      "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/repo/*/user-input/*",
+      "arn:aws:ssm:eu-west-2:${data.aws_caller_identity.current.account_id}:parameter/repo/user-input/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:PutItem*",
+      "dynamodb:GetItem*",
+      "dynamodb:DeleteItem*"
+    ]
+    // FIXME: get table name from ssm
+    resources = ["arn:aws:dynamodb:eu-west-2:${data.aws_caller_identity.current.account_id}:table/prm-deductions-pre-prod-terraform-table"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "bootstrap_admin" {
+  count = var.provision_strict_iam_roles ? 1 : 0
+  policy_arn = data.aws_iam_policy_document.bootstrap_admin_permissions_policy[0].id
+  role = aws_iam_role.bootstrap_admin[0].name
+}
+
+data "aws_caller_identity" "current" {}
+
