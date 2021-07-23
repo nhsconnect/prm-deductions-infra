d99b5d3f57a1de21da0d6b784a07e3d3404f4591
Jack Slater <jslater@thoughtworks.com>
2021-07-23 16:43:46 +0100

    [PRMT-1735] updated BootstrapAdmin role policy create terraform s3 buckets and db

--- terraform-cross-account/iam-strict-environment/iam_users.tf
+++ terraform-cross-account/iam-strict-environment/iam_users.tf
@@ -44,10 +44,32 @@ data "aws_iam_policy_document" "bootstrap_admin_permissions" {
     actions = [
       "dynamodb:PutItem*",
       "dynamodb:GetItem*",
-      "dynamodb:DeleteItem*"
+      "dynamodb:DeleteItem*",
+      "dynamodb:CreateTable",
+      "dynamodb:TagResource",
+      "dynamodb:DeleteTable",
+      "dynamodb:List*",
+      "dynamodb:Describe*"
     ]
     // FIXME: get table name from ssm
     resources = ["arn:aws:dynamodb:eu-west-2:${data.aws_caller_identity.current.account_id}:table/prm-deductions-pre-prod-terraform-table"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutEncryptionConfiguration",
+      "s3:PutLifecycleConfiguration",
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:PutBucketVersioning",
+      "s3:PutBucketTagging",
+      "s3:Get*",
+      "s3:List*"
+    ]
+    resources = [
+      "arn:aws:s3:::prm-deductions-${var.state_bucket_infix}terraform-state*"
+    ]
   }
 
   statement {
