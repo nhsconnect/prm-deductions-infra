1f45da5eca9a1e3351164423a7933fc23cd36d65
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-07-30 17:36:29 +0100

    [PRMT-1735] Add egress sg rule to allow all outbound traffic explicitly

--- terraform/modules/deductions-private/vpn.tf
+++ terraform/modules/deductions-private/vpn.tf
@@ -45,6 +45,14 @@ resource "aws_security_group" "vpn" {
     description = "Client VPN in ${var.environment} env"
     vpc_id      = module.vpc.vpc_id
 
+    egress {
+      description = "Allow All Outbound"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = {
         Name = "${var.environment}-vpn-sg"
         CreatedBy   = var.repo_name
