589419c140f07d49183d6b32e6a8cf05fffa7e13
Jack Slater <jslater@thoughtworks.com>
2021-08-06 16:37:50 +0100

    [PRMT-2035] added permission for boot strap admin to revoke vpn certificate access

--- terraform-cross-account/iam-strict-environment/bootstrap_admin_user.tf
+++ terraform-cross-account/iam-strict-environment/bootstrap_admin_user.tf
@@ -128,7 +128,7 @@ data "aws_iam_policy_document" "bootstrap_admin_permissions" {
 
   statement {
     effect = "Allow"
-    actions =  ["ec2:ExportClientVpnClientConfiguration"]
+    actions =  ["ec2:ExportClientVpnClientConfiguration", "ec2:ImportClientVpnClientCertificateRevocationList"]
     resources = ["arn:aws:ec2:eu-west-2:${data.aws_caller_identity.current.account_id}:client-vpn-endpoint/${data.aws_ssm_parameter.client-vpn-endpoint-id.value}"]
   }
 
