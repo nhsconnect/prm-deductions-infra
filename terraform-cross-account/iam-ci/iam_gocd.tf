f3d6886a344fbc283fdfb5c9dfd2482d4ecfabf8
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-08-11 11:16:38 +0100

    [PRMT-2244] Introduced ci-cross-agent role.

--- terraform-cross-account/iam-ci/iam_gocd.tf
+++ terraform-cross-account/iam-ci/iam_gocd.tf
@@ -27,3 +27,63 @@ resource "aws_iam_role_policy_attachment" "ci_agent" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   role = aws_iam_role.ci_agent.name
 }
+
+resource "aws_iam_role" "ci_cross_agent" {
+  name = "repository-cross-ci-agent"
+  assume_role_policy = data.aws_iam_policy_document.gocd_trust_policy.json
+}
+
+resource "aws_iam_instance_profile" "ci_cross_agent" {
+  name = "repository-cross-ci-agent"
+  role = aws_iam_role.ci_cross_agent.name
+}
+
+resource "aws_iam_role_policy_attachment" "ci_cross_agent" {
+  policy_arn = aws_iam_policy.ci_cross_agent.arn
+  role = aws_iam_role.ci_cross_agent.name
+}
+
+resource "aws_iam_policy" "ci_cross_agent" {
+  name = "CiCrossAgent"
+  policy = data.aws_iam_policy_document.ci_cross_account.json
+}
+
+data "aws_iam_policy_document" "ci_cross_account" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:DescribeRepositories"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:GetRole",
+      "iam:GetPolicy"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeVpcs",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeVpcPeeringConnections"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "route53:GetHostedZone",
+      "route53:ListVPCAssociationAuthorizations",
+      "route53:ListResourceRecordSets",
+      "route53:ListHostedZonesByVPC"
+    ]
+    resources = ["*"]
+  }
+}
