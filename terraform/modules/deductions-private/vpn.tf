be5ee7a98f6bb2908a9d33dd58f88cc0682ab385
Oktawia Kata <oktawia.kata1@nhs.net>
2021-10-07 18:50:47 +0100

    [PRMT-2394] Limit VPN client SG egress to local networks

--- terraform/modules/deductions-private/vpn.tf
+++ terraform/modules/deductions-private/vpn.tf
@@ -46,11 +46,11 @@ resource "aws_security_group" "vpn" {
     vpc_id      = module.vpc.vpc_id
 
     egress {
-      description = "Allow All Outbound"
+      description = "Allow private network outbound"
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ["10.0.0.0/8"]
     }
 
     tags = {
