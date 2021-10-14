182df1fa32ec26cd386554a5a6ccd558898a7990
Dan Moore <dan.moore1@nhs.net>
2021-10-14 13:37:41 +0100

    [PRMT-2401] Trust nhsd-identities to assume RepoDeveloper, BootstrapAdmin
    Co-authored-by: Emily Youngs <emily.youngs1@nhs.net>

--- terraform-cross-account/iam-strict-environment/iam_users.tf
+++ terraform-cross-account/iam-strict-environment/iam_users.tf
@@ -3,7 +3,10 @@ data "aws_iam_policy_document" "trust_policy" {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_ssm_parameter.ci_account_id.value}:role/NHSDAdminRole"]
+      identifiers = [
+        "arn:aws:iam::${data.aws_ssm_parameter.ci_account_id.value}:role/NHSDAdminRole",
+        "arn:aws:iam::${data.aws_ssm_parameter.nhsd_identities_account_id.value}:root"
+      ]
     }
   }
 }
@@ -14,7 +17,10 @@ data "aws_ssm_parameter" "ci_account_id" {
   name = "/repo/ci/user-input/external/aws-account-id"
 }
 
+data "aws_ssm_parameter" "nhsd_identities_account_id" {
+  name = "/repo/nhsd-identities/user-input/external/aws-account-id"
+}
+
 data "aws_ssm_parameter" "client-vpn-endpoint-id" {
   name = "/repo/${var.environment}/output/prm-deductions-infra/client-vpn-endpoint-id"
 }
-
