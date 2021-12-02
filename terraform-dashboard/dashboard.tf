bfc03f8fc0fa2b0e8eba8ec2195ae3d2abd4dfbc
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-12-02 17:01:56 +0300

    [PRMT-2311] Added queue names ssm parameters to use in dashboard json file.

--- terraform-dashboard/dashboard.tf
+++ terraform-dashboard/dashboard.tf
@@ -9,6 +9,9 @@ data "template_file" "widgets" {
   vars = {
     region = var.region,
     environment = var.environment,
-    mesh_forwarder_nems_observability_queue = data.aws_ssm_parameter.mesh_forwarder_nems_observability_queue.value
+    mesh_forwarder_nems_observability_queue = data.aws_ssm_parameter.mesh_forwarder_nems_observability_queue.value,
+#    incoming_nems_events_queue_name = data.aws_ssm_parameter.incoming_nems_events_queue_name.value,
+#    nems_events_dlq_name = data.nems_events_dlq_name,
+#    nems_undhandled_queue_name = data.nems_undhandled_queue_name
   }
 }
--- terraform-dashboard/data.tf
+++ terraform-dashboard/data.tf
@@ -2,3 +2,18 @@ data "aws_ssm_parameter" "mesh_forwarder_nems_observability_queue" {
   name = "/repo/${var.environment}/output/mesh-forwarder/nems-events-observability-queue"
 }
 
+data "aws_ssm_parameter" "incoming_nems_events_queue_name"{
+  name = "/repo/${var.environment}/output/nems-event-processor/incoming-nems-events-queue-name"
+}
+
+data "aws_ssm_parameter" "nems_events_dlq_name"{
+  name = "/repo/${var.environment}/output/nems-event-processor/dlq-name"
+}
+
+data "aws_ssm_parameter" "nems_undhandled_queue_name"{
+  name = "/repo/${var.environment}/output/nems-event-processor/unhandled_events_queue_name"
+}
+
+
+
+
