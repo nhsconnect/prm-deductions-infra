71782cfa6183b30eae11107245c71c6d1d31dc5c
Mario Giampietri <mario.giampietri1@nhs.net>
2022-01-11 17:01:42 +0000

    [PRMT-2468] added policy doc for splunk role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,7 @@ data "aws_ssm_parameter" "splunk_trusted_principal" {
   name = "/repo/user-input/external/splunk-trusted-principal"
 }
 
-data "aws_iam_policy_document" "splunk_loader_trust_policy" {
+data "aws_iam_policy_document" "splunk_trust_policy" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
@@ -12,9 +12,33 @@ data "aws_iam_policy_document" "splunk_loader_trust_policy" {
   }
 }
 
-resource "aws_iam_role" "splunkSQSForwarder" {
-  name               = "SplunkSQSForwarder"
+resource "aws_iam_role" "splunk_sqs_forwarder" {
+  name               = "splunk_sqs_forwarder"
   description        = "Role to allow repo to integrate with splunk"
-  assume_role_policy = data.aws_iam_policy_document.splunk_loader_trust_policy.json
+  assume_role_policy = data.aws_iam_policy_document.splunk_trust_policy.json
 }
 
+resource "aws_iam_role_policy_attachment" "splunk_access_policy_attachment" {
+  role       = aws_iam_role.splunk_sqs_forwarder.name
+  policy_arn = aws_iam_policy.splunk_access_policy.arn
+}
+
+resource "aws_iam_policy" "splunk_access_policy" {
+  name   = "splunk_access_policy"
+  policy = data.aws_iam_policy_document.splunk_access_policy_document.json
+}
+
+data "aws_iam_policy_document" "splunk_access_policy_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:ListQueues",
+      "sqs:ReceiveMessage",
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage",
+      "sqs:DeleteMessage"
+    ]
+    resources = ["**-audit"]
+  }
+}
