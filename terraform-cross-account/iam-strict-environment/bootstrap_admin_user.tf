700b290239d78864f2039500ece52ef2e206f3e0
Mario Giampietri <mario.giampietri1@nhs.net>
2022-02-04 11:44:44 +0000

    [PRMT-2519] added SNS readonly permissions to bootstrap admin

--- terraform-cross-account/iam-strict-environment/bootstrap_admin_user.tf
+++ terraform-cross-account/iam-strict-environment/bootstrap_admin_user.tf
@@ -123,3 +123,8 @@ resource "aws_iam_role_policy_attachment" "ssm_read_only_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
   role = aws_iam_role.bootstrap_admin.name
 }
+
+resource "aws_iam_role_policy_attachment" "sns_read_only_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSReadOnlyAccess"
+  role = aws_iam_role.bootstrap_admin.name
+}
