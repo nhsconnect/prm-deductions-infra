24351f32f56640dfe31eef823130a58d5b158173
Dan Moore <dan.moore1@nhs.net>
2022-03-03 13:06:36 +0000

    Revert "[PRMT-2531] Temporarily commit s3 content denial to dev so can check it"
    This reverts commit f782c97553201194100221dd75eeac7096f38c85.

--- terraform-cross-account/iam-environment/variables.tf
+++ terraform-cross-account/iam-environment/variables.tf
@@ -1,6 +1,1 @@
 variable "provision_strict_iam_roles" {}
-
-
-
-variable "state_bucket_infix" {}
-variable "environment" {}
--- terraform-cross-account/iam.tf
+++ terraform-cross-account/iam.tf
@@ -8,10 +8,6 @@ module "environment_roles" {
   // TODO: Use condition with provision_strict_iam_roles
   count = var.provision_ci_account ? 0 : 1
   provision_strict_iam_roles = var.provision_strict_iam_roles
-
-  environment = var.environment
-  state_bucket_infix = var.state_bucket_infix
-
 }
 
 module "environment_strict_roles" {
