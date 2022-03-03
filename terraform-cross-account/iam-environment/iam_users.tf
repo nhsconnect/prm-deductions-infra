f782c97553201194100221dd75eeac7096f38c85
Dan Moore <dan.moore1@nhs.net>
2022-03-03 12:55:51 +0000

    [PRMT-2531] Temporarily commit s3 content denial to dev so can check it

--- terraform-cross-account/iam-environment/iam_users.tf
+++ terraform-cross-account/iam-environment/iam_users.tf
@@ -39,3 +39,15 @@ data "aws_ssm_parameter" "ci_account_id" {
 data "aws_ssm_parameter" "nhsd_identities_account_id" {
   name = "/repo/nhsd-identities/user-input/external/aws-account-id"
 }
+
+resource "aws_iam_role_policy_attachment" "repo_admin_s3_deny_content_access" {
+  count = var.provision_strict_iam_roles ? 0 : 1
+  policy_arn = aws_iam_policy.s3_deny_content_access.arn
+  role = aws_iam_role.repo_admin[0].name
+}
+
+resource "aws_iam_role_policy_attachment" "repo_admin_s3_allow_terraform_state_content_access" {
+  count = var.provision_strict_iam_roles ? 0 : 1
+  policy_arn = aws_iam_policy.s3_allow_terraform_state_content_access.arn
+  role = aws_iam_role.repo_admin[0].name
+}
--- terraform-cross-account/iam-environment/variables.tf
+++ terraform-cross-account/iam-environment/variables.tf
@@ -1,1 +1,6 @@
 variable "provision_strict_iam_roles" {}
+
+
+
+variable "state_bucket_infix" {}
+variable "environment" {}
