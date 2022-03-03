4d5ec1ce6122fba12c3c919afb3954c2f32f9180
eyupaksu-nhs <eyup.aksu@nhs.net>
2022-03-03 10:42:49 +0000

    [PRMT-2585] Fixed nhs environment in config

--- key-rotation-and-generation/config.js
+++ key-rotation-and-generation/config.js
@@ -1,4 +1,5 @@
 export const initializeConfig = () => ({
   nhsEnvironment: process.env.NHS_ENVIRONMENT,
-  isStrictEnvironment: !(process.env.NHS_ENVIRONMENT === 'dev' || process.env.NHS_ENVIRONMENT === 'test' || process.env.NHS_ENVIRONMENT === 'perf')
+  isStrictEnvironment: !(process.env.NHS_ENVIRONMENT === 'dev' || process.env.NHS_ENVIRONMENT === 'test'
+      || process.env.NHS_ENVIRONMENT === 'pre-prod' || process.env.NHS_ENVIRONMENT === 'perf')
 });
