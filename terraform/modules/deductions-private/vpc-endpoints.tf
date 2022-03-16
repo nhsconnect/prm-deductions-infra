98c45a2db4329509ec405ddeafc4a4795e517e93
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-03-16 14:10:54 +0000

    [PRMT-2599] Added network gateways for dynamodb to vpcs

--- terraform/modules/deductions-private/vpc-endpoints.tf
+++ terraform/modules/deductions-private/vpc-endpoints.tf
@@ -201,7 +201,6 @@ resource "aws_vpc_endpoint" "monitoring" {
     }
 }
 
-
 resource "aws_security_group" "sqs-sg" {
     name = "${var.environment}-${var.component_name}-sqs-endpoint-sg"
     description = "Traffic for the sqs queues VPC endpoint."
@@ -268,36 +267,11 @@ resource "aws_security_group" "monitoring-sg" {
 resource "aws_vpc_endpoint" "dynamodb_gateway_endpoint" {
     vpc_id = module.vpc.vpc_id
     service_name = "com.amazonaws.${var.region}.dynamodb"
-    subnet_ids = module.vpc.private_subnets
     vpc_endpoint_type = "Gateway"
-
-    security_group_ids = [aws_security_group.dynamodb-sg.id]
-
-    private_dns_enabled = true
+    route_table_ids = module.vpc.private_route_table_ids
 
     tags = {
         Name = "${var.environment}-${var.component_name}-dynamo-endpoint"
-        CreatedBy = var.repo_name
-        Environment = var.environment
-    }
-}
-
-resource "aws_security_group" "dynamodb-sg" {
-    name = "${var.environment}-${var.component_name}-dynamodb-endpoint-sg"
-    description = "Traffic for the dynamodb queues VPC endpoint."
-    vpc_id = module.vpc.vpc_id
-
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-
-        # Allow to log from the local network
-        cidr_blocks = [var.cidr]
-    }
-
-    tags = {
-        Name = "${var.environment}-${var.component_name}-dynamodb-sg"
         CreatedBy = var.repo_name
         Environment = var.environment
     }
