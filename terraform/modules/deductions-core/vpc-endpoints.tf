28cf96920198cebf7eeab0ecfca0580ace3c2b14
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-03-17 16:07:41 +0000

    [PRMT-2599] Added s3 vpc prefix list id to use in some security groups in other repos.

--- terraform/modules/deductions-core/vpc-endpoints.tf
+++ terraform/modules/deductions-core/vpc-endpoints.tf
@@ -73,42 +73,6 @@ resource "aws_vpc_endpoint" "cloudwatch-logs" {
   }
 }
 
-resource "aws_vpc_endpoint" "sqs" {
-  vpc_id = module.vpc.vpc_id
-  service_name = "com.amazonaws.${var.region}.sqs"
-
-  subnet_ids = module.vpc.private_subnets
-  vpc_endpoint_type = "Interface"
-
-  security_group_ids = [aws_security_group.sqs-sg.id]
-
-  private_dns_enabled = true
-
-  tags = {
-    Name = "${var.environment}-${var.component_name}-sqs-endpoint"
-    CreatedBy = var.repo_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_vpc_endpoint" "sns" {
-  vpc_id = module.vpc.vpc_id
-  service_name = "com.amazonaws.${var.region}.sns"
-
-  subnet_ids = module.vpc.private_subnets
-  vpc_endpoint_type = "Interface"
-
-  security_group_ids = [aws_security_group.sns-sg.id]
-
-  private_dns_enabled = true
-
-  tags = {
-    Name = "${var.environment}-${var.component_name}-sns-endpoint"
-    CreatedBy = var.repo_name
-    Environment = var.environment
-  }
-}
-
 resource "aws_vpc_endpoint" "monitoring" {
   vpc_id = module.vpc.vpc_id
   service_name = "com.amazonaws.${var.region}.monitoring"
@@ -126,62 +90,6 @@ resource "aws_vpc_endpoint" "monitoring" {
     Environment = var.environment
   }
 }
-
-resource "aws_vpc_endpoint" "dynamodb_gateway_endpoint" {
-  vpc_id = module.vpc.vpc_id
-  service_name = "com.amazonaws.${var.region}.dynamodb"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids = module.vpc.private_route_table_ids
-
-  tags = {
-    Name = "${var.environment}-${var.component_name}-dynamo-endpoint"
-    CreatedBy = var.repo_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_security_group" "sqs-sg" {
-  name = "${var.environment}-${var.component_name}-sqs-endpoint-sg"
-  description = "Traffic for the sqs queues VPC endpoint."
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-
-    # Allow to log from the local network
-    cidr_blocks = [var.cidr]
-  }
-
-  tags = {
-    Name = "${var.environment}-${var.component_name}-sqs-sg"
-    CreatedBy = var.repo_name
-    Environment = var.environment
-  }
-}
-
-resource "aws_security_group" "sns-sg" {
-  name = "${var.environment}-${var.component_name}-sns-endpoint-sg"
-  description = "Traffic for the sns VPC endpoint."
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-
-    # Allow to log from the local network
-    cidr_blocks = [var.cidr]
-  }
-
-  tags = {
-    Name = "${var.environment}-${var.component_name}-sns-endpoint-sg"
-    CreatedBy = var.repo_name
-    Environment = var.environment
-  }
-}
-
 
 resource "aws_security_group" "logs-endpoint-sg" {
   name = "${var.environment}-${var.component_name}-logs-endpoint-sg"
@@ -275,4 +183,14 @@ resource "aws_security_group" "monitoring-sg" {
     CreatedBy = var.repo_name
     Environment = var.environment
   }
+}
+
+data "aws_prefix_list" "s3" {
+  prefix_list_id = aws_vpc_endpoint.s3.prefix_list_id
+}
+
+resource "aws_ssm_parameter" "s3_prefix_list_id" {
+  name = "/repo/${var.environment}/output/${var.repo_name}/s3_prefix_list_id"
+  type = "String"
+  value = data.aws_prefix_list.s3.id
 }
