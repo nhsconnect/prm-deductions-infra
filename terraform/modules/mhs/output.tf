e4ecabb1b3a30d60e994ed583e95daf351896c30
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-03-18 16:39:25 +0000

    [PRMT-2599] output mhs vpc id to use in sg in outside of this repo.

--- terraform/modules/mhs/output.tf
+++ terraform/modules/mhs/output.tf
@@ -5,3 +5,13 @@ output "vpc_id" {
 output "private_mhs_vpc_peering_id" {
   value = aws_vpc_peering_connection.private_mhs.id
 }
+
+resource "aws_ssm_parameter" "deductions_core_vpc_id" {
+  name = "/repo/${var.environment}/output/${var.repo_name}/mhs-vpc-id"
+  type = "String"
+  value = local.mhs_vpc_id
+  tags = {
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
