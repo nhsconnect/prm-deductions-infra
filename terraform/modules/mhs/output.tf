c938bb04426b378d1a083a8083363d39b425461d
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-03-21 12:09:42 +0000

    [PRMT-2599] Remove ssm param since we can filter vpc id via tag.

--- terraform/modules/mhs/output.tf
+++ terraform/modules/mhs/output.tf
@@ -5,13 +5,3 @@ output "vpc_id" {
 output "private_mhs_vpc_peering_id" {
   value = aws_vpc_peering_connection.private_mhs.id
 }
-
-resource "aws_ssm_parameter" "mhs_vpc_id" {
-  name = "/repo/${var.environment}/output/${var.repo_name}/mhs-vpc-id"
-  type = "String"
-  value = local.mhs_vpc_id
-  tags = {
-    CreatedBy   = var.repo_name
-    Environment = var.environment
-  }
-}
