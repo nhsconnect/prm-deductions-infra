70651bb819a4ee05998f535afa8376047b7d550c
Mario Giampietri <mario.giampietri1@nhs.net>
2022-06-20 11:41:57 +0100

    [PRMT-1565] created empty repository dashboard

---
+++ terraform-dashboard/dashboard_repository.tf
@@ -0,0 +1,28 @@
+locals {
+#  repo_all_widgets = concat(
+##  [module.health_widgets["re_registration_service"].widget]
+#  )
+
+#  re_registration_service = {
+#    name  = "re-registration-service"
+#    title = "Re-registration Service"
+#  }
+
+#  repo_task_widget_components  = [
+#    local.re_registration_service
+#  ]
+#  repo_task_widget_types       = ["cpu", "memory"]
+#  repo_task_widget_definitions = [
+#  for pair in setproduct(local.repo_task_widget_types, local.repo_task_widget_components) : {
+#    component = pair[1]
+#    type      = pair[0]
+#  }
+#  ]
+}
+
+resource "aws_cloudwatch_dashboard" "repository_dashboard" {
+  dashboard_body = jsonencode({
+#    widgets = local.repo_all_widgets
+  })
+  dashboard_name = "RepositoryDashboard${title(var.environment)}"
+}
