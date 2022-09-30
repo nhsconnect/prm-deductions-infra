969a105871e1bb938c05c82eab03ae4555947189
eyupaksu-nhs <eyup.aksu@thoughtworks.com>
2022-09-30 13:04:59 +0100

    [PRMT-TECH] Introduved another rds cluster group for new postgres version.

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -18,3 +18,24 @@ resource "aws_ssm_parameter" "repo_databases" {
     Environment = var.environment
   }
 }
+
+resource "aws_rds_cluster_parameter_group" "repo_databases_version_13" {
+  name   = "repo-databases-version-13"
+  family = "aurora-postgresql13"
+
+  parameter {
+    name  = "ssl_min_protocol_version"
+    value = "TLSv1.2"
+  }
+}
+
+resource "aws_ssm_parameter" "repo_databases_version_13" {
+  name = "/repo/${var.environment}/output/${var.repo_name}/repo-databases-parameter-group-name-version-13"
+  type = "String"
+  value = aws_rds_cluster_parameter_group.repo_databases_version_13.name
+
+  tags = {
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
