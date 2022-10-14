f5d60df5986c8bef366bf3fc8040821672afc2d3
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-10-14 13:54:35 +0100

    [PRMT-2890]: [PEN-TEST-SECURITY-AWS-14]: Disable cleartext communication for all S3 buckets.
    Co-authored-by: romanenhs <romane.castera1@nhs.net>

--- terraform/cost.tf
+++ terraform/cost.tf
@@ -5,11 +5,74 @@ resource "aws_s3_bucket" "cost_and_usage_bucket" {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
+        kms_master_key_id = aws_kms_key.cost_and_usage_kms_key.arn
       }
     }
   }
   tags = {
     CreatedBy   = var.repo_name
     Environment = var.environment
+  }
+}
+
+resource "aws_kms_key" "cost_and_usage_kms_key" {
+  description = "Custom KMS Key to enable server side encryption for cost and usage bucket"
+  policy      = data.aws_iam_policy_document.kms_key_policy_doc.json
+  enable_key_rotation = true
+
+  tags = {
+    Name        = "${var.environment}-${var.repo_name}-cost-and-usage-kms-key"
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_kms_alias" "cost_and_usage_encryption" {
+  name          = "alias/cost-and-usage-encryption-kms-key"
+  target_key_id = aws_kms_key.cost_and_usage_kms_key.id
+}
+
+data "aws_iam_policy_document" "kms_key_policy_doc" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type        = "AWS"
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["cloudwatch.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = ["*"]
   }
 }
