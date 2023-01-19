2e64cc4cdc0e51e9794463e7cc0f580b9874df43
Shashin Dayanand <shashin.dayanand@thoughtworks.com>
2023-01-19 09:26:20 +0000

    [PRMT-2900] Send any errors to support team via email

--- generate-cost-report-lambda/main.py
+++ generate-cost-report-lambda/main.py
@@ -1,7 +1,9 @@
 import datetime
+import logging
 import os
 import re
 import time
+import traceback
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -11,7 +13,7 @@ import boto3
 import botocore
 import yaml
 from botocore.exceptions import ClientError
-import logging
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -52,8 +54,10 @@ if not region:
     region = cfg['region']
 sender_email_ssm_parameter = cfg['sender_email_ssm_parameter']
 recipient_email_ssm_parameter = cfg['recipient_email_ssm_parameter']
-sender = get_ssm_parameter(sender_email_ssm_parameter)['Parameter']['Value']
-recipient = get_ssm_parameter(recipient_email_ssm_parameter)['Parameter']['Value']
+sender_email = get_ssm_parameter(sender_email_ssm_parameter)['Parameter']['Value']
+recipient_emails = get_ssm_parameter(recipient_email_ssm_parameter)['Parameter']['Value']
+# TODO add support email SSM parameter
+support_email_address = get_ssm_parameter(sender_email_ssm_parameter)['Parameter']['Value']
 athena_queries = cfg['query_string_list']
 queries_to_execute = []
 tempPath = '/tmp'
@@ -101,6 +105,7 @@ def get_var_char_values(d):
             rowList.append("")
     return rowList
 
+
 def execute_cur_queries_on_athena():
     client = boto3.client('athena', region_name=region)
     logger.info("Starting CUR query execution ... ")
@@ -113,7 +118,8 @@ def execute_cur_queries_on_athena():
             })
         queries_to_execute[query_index]['queryId'] = resp['QueryExecutionId']
         logger.info(
-            "Query to execute is: " + queries_to_execute[query_index]['queryString'] + " QueryId is: " + queries_to_execute[query_index][
+            "Query to execute is: " + queries_to_execute[query_index]['queryString'] + " QueryId is: " +
+            queries_to_execute[query_index][
                 'queryId'])
 
     wait_for_query_execution_seconds = 5
@@ -127,19 +133,19 @@ def execute_cur_queries_on_athena():
         status = response_get_query_details['QueryExecution']['Status']['State']
         if (status == AthenaQueryExecutionStatus.FAILED) or (status == AthenaQueryExecutionStatus.CANCELLED):
             failure_reason = response_get_query_details['QueryExecution']['Status']['StateChangeReason']
-            logger.error("Query either FAILED or CANCELED, reason is: " + failure_reason)
-            #Add new function here for handling failure
-            return False, False
+            logger.error("Query either FAILED or CANCELED, reason is: " + failure_reason, exc_info=True)
+            send_error_details_to_support("Athena query either FAILED or CANCELED with reason " + failure_reason)
+            #TODO raise user exception
+            raise
 
         elif status == AthenaQueryExecutionStatus.QUEUED:
+            logger.info('Query queued, waiting for query execution to begin')
             time.sleep(wait_for_query_execution_seconds)
-            logger.info('Waiting 100ms for query execution!')
 
         elif status == AthenaQueryExecutionStatus.SUCCEEDED:
-            location = response_get_query_details['QueryExecution']['ResultConfiguration']['OutputLocation']
             logger.info("Query execution succeeded, the query ID is: " + resp['QueryExecutionId'])
+            location = response_get_query_details['QueryExecution']['ResultConfiguration']['OutputLocation']
 
-            # Function to get output results
             response_query_result = client.get_query_results(
                 QueryExecutionId=resp['QueryExecutionId']
             )
@@ -150,45 +156,49 @@ def execute_cur_queries_on_athena():
                 header = [obj['VarCharValue'] for obj in header['Data']]
                 logger.info("Query result output location is: " + location)
                 result = [dict(zip(header, get_var_char_values(row))) for row in rows]
-
-                return location, result
             else:
                 logger.info("No results found!")
-                return location, None
-
-    return False
 
 
 def fetch_cost_report_into_lambda_directory(bucket_name, key_path, query_list):
     os.chdir(tempPath)
     s3 = boto3.resource('s3')
     for i in range(len(query_list)):
-        logger.info("Copying query result from output source location: s3://" + bucket_name + "/" + key_path + query_list[i]['queryId'] + ".csv")
+        logger.info(
+            "Copying query result from output source location: s3://" + bucket_name + "/" + key_path + query_list[i][
+                'queryId'] + ".csv")
         try:
-            s3.Bucket(bucket_name).download_file(key_path + query_list[i]['queryId'] + '.csv',
-                                                 file_name)
+            s3.Bucket(bucket_name).download_file(key_path + query_list[i]['queryId'] + '.csv', file_name)
             is_file_downloaded = os.path.isfile('/tmp/' + file_name)
             logger.info(f"Successfully copied the report:  {is_file_downloaded}")
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
-                logger.error("The target query result file does not exist.")
-                #Send email to support team
+                logger.error("The target query result file does not exist.", exc_info=True)
+                send_error_details_to_support("The query result file does not exist")
             else:
                 raise
 
 
-def send_report_via_email(ses_region, email_subject, email_sender, email_receivers, report_name, email_body):
+def send_error_details_to_support(error_message):
+    email_body = f"There was an error generating the cost and usage report. Please find details below: \n\n" \
+                 f"Error description: {error_message}\n\n" \
+                 f"Error stack trace:\n" \
+                 f"{traceback.format_exc()} "
+    send_email("Error generating PRM cost and usage report", sender_email, support_email_address, None, email_body)
+
+
+def send_email(email_subject, email_sender, email_receivers, report_name, email_body):
+    logger.info("Sending email via SES")
     os.chdir(tempPath)
-    logger.info("Emailing cost and usage report via SES... ")
-    client = boto3.client('ses', region_name=ses_region)
+    client = boto3.client('ses', region_name=region)
     message = MIMEMultipart('mixed')
     message['Subject'] = email_subject
     message['From'] = email_sender
     message['To'] = email_receivers
-
-    email_attachment = MIMEApplication(open(report_name, 'rb').read())
-    email_attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(report_name))
-    message.attach(email_attachment)
+    if report_name:
+        email_attachment = MIMEApplication(open(report_name, 'rb').read())
+        email_attachment.add_header('Content-Disposition', 'attachment', filename=os.path.basename(report_name))
+        message.attach(email_attachment)
     message.attach(MIMEText(email_body))
     try:
         response = client.send_raw_email(
@@ -200,14 +210,18 @@ def send_report_via_email(ses_region, email_subject, email_sender, email_receive
         )
         return response
     except ClientError as e:
-        logger.error(e.response['Error']['Message'])
-        #Send email to support team
+        logger.error(e.response['Error']['Message'], exc_info=True)
 
 
 def lambda_handler(event, context):
-    execute_cur_queries_on_athena()
-    fetch_cost_report_into_lambda_directory(cur_bucket, cur_key_path, queries_to_execute)
-    response = send_report_via_email(region, subject, sender, recipient, file_name, body_text)
-    return response
+    try:
+        execute_cur_queries_on_athena()
+        fetch_cost_report_into_lambda_directory(cur_bucket, cur_key_path, queries_to_execute)
+    except Exception as e:
+        send_error_details_to_support("Unexpected exception while generating cost and usage report")
+    else:
+        response = send_email(subject, sender_email, recipient_emails, file_name, body_text)
+        return response
+
 
 lambda_handler(None, None)
