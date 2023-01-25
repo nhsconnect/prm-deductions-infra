e963543ddf3a831cbfb3f0597d96956d9a7bcf3e
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2023-01-25 12:16:29 +0000

    [PRMT-2900] Created AWS SES Identities for receiver email list.
    Co-authored-by: Jessy Yeh <jessy.yeh@thoughtworks.com>

--- terraform/ses-email-identities.tf
+++ terraform/ses-email-identities.tf
@@ -1,3 +1,13 @@
+locals {
+  receiver_email_address_list = split(",", nonsensitive(data.aws_ssm_parameter.receiver_cost_report_email_id.value))
+}
+
+
 resource "aws_ses_email_identity" "create_cost_report_sender_email_identity" {
   email = data.aws_ssm_parameter.sender_cost_report_email_id.value
+}
+
+resource "aws_ses_email_identity" "create_cost_report_receiver_email_ident" {
+  for_each = toset(local.receiver_email_address_list)
+  email = each.value
 }
