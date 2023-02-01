6e977b97190e7baf74b6b1ce3725da5d30ee6abe
Rosie Davidson <rosie.amphlett1@nhs.net>
2023-02-01 12:11:42 +0000

    [PRMT-2900] Testing removal of email identities in terraform as verification emails are sent on every deployment

--- terraform/ses-email-identities.tf
+++ terraform/ses-email-identities.tf
@@ -4,16 +4,16 @@ locals {
 }
 
 
-resource "aws_ses_email_identity" "create_cost_report_sender_email_identity" {
-  email = data.aws_ssm_parameter.sender_cost_report_email_id.value
-}
-
-resource "aws_ses_email_identity" "create_cost_report_receiver_email_identity" {
-  for_each = toset(local.receiver_email_address_list)
-  email = each.value
-}
-
-resource "aws_ses_email_identity" "create_cost_support_email_identity" {
-  for_each = toset(local.support_email_address_list)
-  email = each.value
-}
+#resource "aws_ses_email_identity" "create_cost_report_sender_email_identity" {
+#  email = data.aws_ssm_parameter.sender_cost_report_email_id.value
+#}
+#
+#resource "aws_ses_email_identity" "create_cost_report_receiver_email_identity" {
+#  for_each = toset(local.receiver_email_address_list)
+#  email = each.value
+#}
+#
+#resource "aws_ses_email_identity" "create_cost_support_email_identity" {
+#  for_each = toset(local.support_email_address_list)
+#  email = each.value
+#}
