122d66c08736850ada903523d6a8a03b26d7a057
Rosie Davidson <rosie.amphlett1@nhs.net>
2023-02-08 15:08:26 +0000

    [PRMT-3114] Created s3 bucket policy to allow load balancers to publish access logs

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -90,6 +90,22 @@ resource "aws_s3_bucket" "alb_access_logs" {
   }
 }
 
+resource "aws_s3_bucket_policy" "alb_access_logs_policy" {
+  bucket = aws_s3_bucket.alb_access_logs.id
+  policy = data.aws_iam_policy_document.allow_load_balancers_to_publish_to_access_logs_s3_bucket.json
+}
+
+data "aws_iam_policy_document" "allow_load_balancers_to_publish_to_access_logs_s3_bucket" {
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::652711504416:root"]
+    }
+    actions = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.alb_access_logs.arn}/*"]
+  }
+}
+
 resource "aws_ssm_parameter" "alb_access_logs_s3_bucket_id" {
   value = aws_s3_bucket.alb_access_logs.id
   type = "String"
