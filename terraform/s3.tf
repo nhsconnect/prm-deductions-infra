9bd7803e2a2bc6fb595103665d5e64ec48ddf155
Kris Bloe <kris.bloe@answerdigital.com>
2024-02-19 12:20:40 +0000

    Formatting

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -80,102 +80,102 @@ resource "aws_s3_bucket_public_access_block" "cost_and_usage_access_logs" {
 }
 
 resource "aws_s3_bucket_policy" "cost_usage_permit_developer_to_see_access_logs_policy" {
- count  = var.is_restricted_account ? 1 : 0
- bucket = aws_s3_bucket.cost_and_usage_access_logs.id
- policy = jsonencode({
-   "Statement" : [
-     {
-       Effect : "Allow",
-       Principal : {
-         "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RepoDeveloper"
-       },
-       Action : ["s3:Get*", "s3:ListBucket"],
-       Resource : [
-         "${aws_s3_bucket.cost_and_usage_access_logs.arn}",
-         "${aws_s3_bucket.cost_and_usage_access_logs.arn}/*"
-       ],
-       Condition : {
-         Bool : {
-           "aws:SecureTransport" : "false"
-         }
-       }
-     }
-   ]
- })
+  count  = var.is_restricted_account ? 1 : 0
+  bucket = aws_s3_bucket.cost_and_usage_access_logs.id
+  policy = jsonencode({
+    "Statement" : [
+      {
+        Effect : "Allow",
+        Principal : {
+          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/RepoDeveloper"
+        },
+        Action : ["s3:Get*", "s3:ListBucket"],
+        Resource : [
+          "${aws_s3_bucket.cost_and_usage_access_logs.arn}",
+          "${aws_s3_bucket.cost_and_usage_access_logs.arn}/*"
+        ],
+        Condition : {
+          Bool : {
+            "aws:SecureTransport" : "false"
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_policy" "cost_usage_permit_s3_to_write_access_logs_policy" {
- bucket = aws_s3_bucket.cost_and_usage_access_logs.id
- policy = jsonencode({
-   "Version" : "2012-10-17",
-   "Statement" : [
-     {
-       "Sid" : "S3ServerAccessLogsPolicy",
-       "Effect" : "Allow",
-       "Principal" : {
-         "Service" : "logging.s3.amazonaws.com"
-       },
-       "Action" : "s3:PutObject",
-       "Resource" : "${aws_s3_bucket.cost_and_usage_access_logs.arn}/${local.cost_usage_access_logs_prefix}*",
-       Condition : {
-         Bool : {
-           "aws:SecureTransport" : "false"
-         }
-       }
-     }
-   ]
- })
+  bucket = aws_s3_bucket.cost_and_usage_access_logs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "S3ServerAccessLogsPolicy",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "logging.s3.amazonaws.com"
+        },
+        "Action" : "s3:PutObject",
+        "Resource" : "${aws_s3_bucket.cost_and_usage_access_logs.arn}/${local.cost_usage_access_logs_prefix}*",
+        Condition : {
+          Bool : {
+            "aws:SecureTransport" : "false"
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket" "alb_access_logs" {
- bucket = "${var.environment}-repo-load-balancer-access-logs"
+  bucket = "${var.environment}-repo-load-balancer-access-logs"
 
- tags = {
-   CreatedBy   = var.repo_name
-   Environment = var.environment
- }
+  tags = {
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "alb_access_logs" {
- bucket = aws_s3_bucket.alb_access_logs.bucket
+  bucket = aws_s3_bucket.alb_access_logs.bucket
 
- block_public_acls       = true
- block_public_policy     = true
- ignore_public_acls      = true
- restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "alb_access_logs" {
- count = var.s3_backup_enabled ? 1 : 0
+  count = var.s3_backup_enabled ? 1 : 0
 
- bucket = aws_s3_bucket.alb_access_logs.bucket
+  bucket = aws_s3_bucket.alb_access_logs.bucket
 
- versioning_configuration {
-   status = "Enabled"
- }
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_policy" "alb_access_logs_policy" {
- bucket = aws_s3_bucket.alb_access_logs.id
- policy = data.aws_iam_policy_document.allow_load_balancers_to_publish_to_access_logs_s3_bucket.json
+  bucket = aws_s3_bucket.alb_access_logs.id
+  policy = data.aws_iam_policy_document.allow_load_balancers_to_publish_to_access_logs_s3_bucket.json
 }
 
 data "aws_iam_policy_document" "allow_load_balancers_to_publish_to_access_logs_s3_bucket" {
- statement {
-   principals {
-     type        = "AWS"
-     identifiers = ["arn:aws:iam::652711504416:root"]
-   }
-   actions   = ["s3:PutObject"]
-   resources = ["${aws_s3_bucket.alb_access_logs.arn}/*"]
- }
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::652711504416:root"]
+    }
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.alb_access_logs.arn}/*"]
+  }
 }
 
 resource "aws_ssm_parameter" "alb_access_logs_s3_bucket_id" {
- value       = aws_s3_bucket.alb_access_logs.id
- type        = "String"
- description = "Exported this bucket id so each alb in different git repos can configure logs"
- name        = "/repo/${var.environment}/output/${var.repo_name}/alb-access-logs-s3-bucket-id"
+  value       = aws_s3_bucket.alb_access_logs.id
+  type        = "String"
+  description = "Exported this bucket id so each alb in different git repos can configure logs"
+  name        = "/repo/${var.environment}/output/${var.repo_name}/alb-access-logs-s3-bucket-id"
 }
 
 resource "aws_s3_bucket" "access_logs" {
