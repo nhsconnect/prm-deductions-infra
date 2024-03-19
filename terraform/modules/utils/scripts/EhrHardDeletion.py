155a23d07698e8abb403693666f1e72099a91d77
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2024-03-19 13:52:52 +0000

    [PRMT-4391] - Added log comments and exceptions

--- terraform/modules/utils/scripts/EhrHardDeletion.py
+++ terraform/modules/utils/scripts/EhrHardDeletion.py
@@ -7,7 +7,7 @@ from boto3.dynamodb.conditions import Key
 def lambda_handler(event, context):
     dynamodbTable, inboundConversationId = parseEvent(event)
     deleteEhrFromS3(inboundConversationId)
-    verifyDBTableRecordsDeleted(dynamodbTable, inboundConversationId)
+    verify_database_table_records_deleted(dynamodbTable, inboundConversationId)
 
 def deleteEhrFromS3(inboundConversationId):
     s3 = boto3.resource('s3')
@@ -19,8 +19,10 @@ def deleteEhrFromS3(inboundConversationId):
         repoBucket = s3.Bucket(s3BucketName)
     except KeyError as error:
         print("Failed to get S3_REPO_BUCKET environment variable: " + error)
+        # Log to splunk for monitoring
     except Exception as error:
         print("Failed to find the S3 Bucket: " + error)
+        # Log to splunk for monitoring
 
     if any(True for _ in repoBucket.objects.filter(Prefix=inboundConversationId + "/")): # Checks to see objects exist in the S3 Bucket for the given inboundConversationId
         print('EHR found in the S3 Bucket')
@@ -29,10 +31,9 @@ def deleteEhrFromS3(inboundConversationId):
             repoBucket.objects.filter(Prefix=inboundConversationId + "/").delete()
             if all(False for _ in repoBucket.objects.filter(Prefix=inboundConversationId + "/")):
                 print("EHR has been deleted from the S3 Bucket successfully!")
-            else:
-                print("EHR deletion was unsuccessful")
         except Exception as error:
             print("Failed to delete EHR in the S3 Bucket: " + error)
+            # Log to splunk for monitoring
     else:
         print("EHR could not be found in the S3 Bucket")
 
@@ -44,16 +45,25 @@ def parseEvent(event):
         inboundConversationId = event["Records"][0]["dynamodb"]["Keys"]["InboundConversationId"]["S"]
     except KeyError as error:
         print("Could not find the relevant event key(s): " + error)
+        # Log to splunk for monitoring
 
-    print("InboundConversationId: " + inboundConversationId)
-    print("DynamoDB Table: " + dynamodbTable)
-    print("Deleted Conversation - " + str(deletedTableRecord))
+    print(
+        "InboundConversationId: " + inboundConversationId +
+        ", DynamoDB Table: " + dynamodbTable +
+        ", Deleted Conversation: " + str(deletedTableRecord)
+    )
     return dynamodbTable, inboundConversationId
 
-def verifyDBTableRecordsDeleted(dynamodbTable, inboundConversationId):
+def verify_database_table_records_deleted(dynamodbTable: str, inboundConversationId: str) -> None:
     dynamodb = boto3.resource('dynamodb')
     print("Retrieving DynamoDB table")
     ehrTrasferTrackerTable = dynamodb.Table(dynamodbTable)
-    print("Verifying database records have been deleted")
+    print("Verifying all database records have been deleted")
     queryResponse = ehrTrasferTrackerTable.query(KeyConditionExpression=Key("InboundConversationId").eq(inboundConversationId))
-    print(queryResponse["Count"])
+
+    if queryResponse["Count"] == 0:
+        print("All database records have been deleted")
+    else:
+        print("Number of database records still existing: " + str(queryResponse["Count"]))
+        raise Exception("[WTF] - Database records still exist for InboundConversationId: " + inboundConversationId)
+        # Log to splunk for monitoring
